8860b83fa464bb45bb1abd5542b6d04a636a0828
donayesh.nanayakkara@circles.asia <donayesh.nanayakkara@circles.asia>
2023-10-10 15:13:55 +0530

    Initial commit

---
+++ country-code-picker/src/main/java/com/don/country_code_picker/CountryCodeEditText.kt
@@ -0,0 +1,14 @@
+package com.don.country_code_picker
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CountryCodeEditText( modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Text from module!",
+        modifier = modifier,
+    )
+
+}
